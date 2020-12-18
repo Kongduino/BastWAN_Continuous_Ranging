373ec3c56d36577e4dec897f92f9e5ce486cc56d
Kongduino <dbvic@me.com>
2020-12-18 16:19:14 +0800

    New version with MLX90614 IR thermometer

--- BastWAN_Continuous_Ranging.ino
+++ BastWAN_Continuous_Ranging.ino
@@ -1,9 +1,11 @@
 #include "Seeed_vl53l0x.h"
 #include "DHT12.h"
 #include <Wire.h> //The DHT12 uses I2C comunication.
+#include <SparkFunMLX90614.h> //Click here to get the library: http://librarymanager/All#Qwiic_IR_Thermometer by SparkFun
 
 DHT12 dht12; //Preset scale CELSIUS and ID 0x5c.
 Seeed_vl53l0x VL53L0X;
+IRTherm therm; // Create an IRTherm object to interact with throughout
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
 #define SERIAL SerialUSB
@@ -15,10 +17,10 @@ uint8_t hitCounts = 0;
 void setup() {
   analogReference(AR_DEFAULT); //set the reference voltage 1.1V,the distinguishability can up to 1mV.
   VL53L0X_Error Status = VL53L0X_ERROR_NONE;
-  Serial.begin(115200);
-  Serial.flush();
+  SERIAL.begin(115200);
+  SERIAL.flush();
   delay(3000);
-  Serial.print("\n\nContinuous ranging...");
+  SERIAL.println("\n\nContinuous ranging...");
   pinMode(A0, OUTPUT);
   analogWrite(A0, 0);
   Status = VL53L0X.VL53L0X_common_init();
@@ -33,6 +35,21 @@ void setup() {
     VL53L0X.print_pal_error(Status);
     while (1);
   }
+  Wire.begin(); //Join I2C bus
+  SERIAL.print("IR thermometer ");
+  if (therm.begin() == false) {
+    // Initialize thermal IR sensor
+    SERIAL.println("did not acknowledge! Freezing!");
+    while (1);
+  }
+  SERIAL.println("acknowledged.");
+  therm.setUnit(TEMP_C); // Set the library's units to Farenheit
+  if (therm.readID()) {
+    // Read from the ID registers
+    // If the read succeeded, print the ID:
+    Serial.println("ID: 0x" + String(therm.getIDH(), HEX) + String(therm.getIDL(), HEX));
+  }
+  Serial.println(String(therm.readEmissivity()));
 }
 
 void loop() {
@@ -40,28 +57,38 @@ void loop() {
   VL53L0X_RangingMeasurementData_t RangingMeasurementData;
   VL53L0X.PerformContinuousRangingMeasurement(&RangingMeasurementData);
   if (RangingMeasurementData.RangeMilliMeter >= 2000) {
-    SERIAL.println("out of range");
+    //SERIAL.println("out of range");
   } else {
     ds = RangingMeasurementData.RangeMilliMeter / 10.0;
-    if (ds < 45.0) {
+    if (ds < 15.0) {
       hitCounts += 1;
       if (hitCounts == 10) {
         SERIAL.print("distance: ");
         SERIAL.print(ds);
         SERIAL.println(" cm");
         analogWrite(A0, 255);
-        Serial.println("Switching on!");
-        Serial.print(" . T: "); Serial.print(dht12.readTemperature()); Serial.println(" C");
-        Serial.print(" . H: "); Serial.print(dht12.readHumidity()); Serial.println("%");
+        SERIAL.println("\nSwitching on!");
+        SERIAL.print(" . T: "); SERIAL.print(dht12.readTemperature()); SERIAL.println(" C");
+        SERIAL.print(" . H: "); SERIAL.print(dht12.readHumidity()); SERIAL.println("%");
+        if (therm.read()) {
+          // On success, read() will return 1, on fail 0.
+          // Use the object() and ambient() functions to grab the object and ambient
+          // temperatures.
+          // They'll be floats, calculated out to the unit you set with setUnit().
+          Serial.print("Object: " + String(therm.object(), 2));
+          Serial.println("* C");
+          Serial.print("Ambient: " + String(therm.ambient(), 2));
+          Serial.println("* C");
+        }
       }
     } else {
-      SERIAL.print("distance: ");
-      SERIAL.print(ds);
-      SERIAL.println(" cm");
+      // SERIAL.print("distance: ");
+      // SERIAL.print(ds);
+      // SERIAL.println(" cm");
       if (hitCounts > 0) {
         analogWrite(A0, 0);
         hitCounts = 0;
-        Serial.println("Switching off!");
+        SERIAL.println("\nSwitching off!");
       }
     }
   }
--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 A project demonstrating I2C capabilities of the BastWAN.
 
-It requires 2 libraries, one of which is included in the code:
+It requires 3 libraries, one of which is included in the code:
 
 * `DHT12.c/h` I2C version of the DHT11.
 * `Seeed_vl53l0x.h` The driver for the Grove - [Time of Flight Distance Sensor](https://wiki.seeedstudio.com/Grove-Time_of_Flight_Distance_Sensor-VL53L0X/).
+* [`SparkFunMLX90614`](http://librarymanager/All#Qwiic_IR_Thermometer) for the MLX90614 infrared thermometer.
 
 An I2C hub is connected to the SDA/SCL pins, plus GND/Vcc. On this hub are the VL53L0X and DHT12. An LED is connected on A0 + GND/Vcc.
 
-Using the `Continuous Ranging.ino` sketch I repurposed it to activate the DHT12 code when the detected range is below 45 cm. To avoid accidental triggers, it wait for 10 continuous hits, then turns on the LED, and displays the temperature and humidity. It then waits for the VL53L0X to report a measure distance above 45 cm.
+Using the `Continuous Ranging.ino` sketch I repurposed it to activate the DHT12 code when the detected range is below 10 cm. To avoid accidental triggers, it wait for 10 continuous hits, then turns on the LED, and displays the ambient temperature and humidity from DHT12, and the ambient and object temperatures from the MLX90614. It then waits for the VL53L0X to report a measure distance above 10 cm to release and start watching again.
 
 One could add sending a LoRa packet at this point. I'll leave this to you as an exercice. :-)
 
@@ -20,3 +21,5 @@ One could add sending a LoRa packet at this point. I'll leave this to you as an 
 ![VL53L0X](photos/VL53L0X.jpg)
 
 ![Green LED](photos/Green_LED.jpg)
+
+![MLX90614](photos/MLX90614.jpg)
